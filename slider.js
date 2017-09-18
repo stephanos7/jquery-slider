12fdea6a3d30577aa19499ec8843ea22d900bbff
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-09-19 00:02:33 +0200

    add vertically aligned images fitted within carousel container. add initial onclick function for next items and check for end of loop

---
+++ slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  console.log("welcome");
+  //set options for slider
+  var speed = 300;
+  var autoswitch = true;    
+  var autoswitchSpeed = 2000;
+
+  //add active class
+  $('.carousel-img').first().addClass('active');
+  //hide all other slides
+  $('.carousel-img').hide();
+  //show first slide
+  $('.active').show();
+  //go forwards
+  $('#next').on('click', function(){
+    $('.active').removeClass('active').addClass('lastActive');
+    if($('.lastActive').is(':last-child')){
+      $('.carousel-img').first().addClass('active');
+    }else{
+      $('.lastActive').next().addClass('active');
+    }
+    $('.lastActive').removeClass('lastActive');
+    $('.carousel-img').fadeOut(speed);
+    $('.active').fadeIn(speed);
+  })
+});
